76daec6f06fc0c2f36af6cd889c2ab45a15f1877
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2017-06-21 15:21:54 +0200

    test file upload

---
+++ src/main/java/test/s12478_projekt_X.java
@@ -0,0 +1,12 @@
+package test;
+
+public class s12478_projekt_X {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[][][] mySort(int[] dane){
+        return null;
+    }
+}
+
